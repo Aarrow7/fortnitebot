32be1a68385a99eb871f4f8fe95cf024d944b7c6
Aarrow7 <35619644+Aarrow7@users.noreply.github.com>
2018-02-09 13:41:37 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -21,10 +21,7 @@ SOFTWARE.
 import discord
 import os
 from discord.ext import commands
-import pynite
 
-frtoken = os.environ.get('TRN-API-KEY')
-frclient = pynite.Client(frtoken, timeout=5)
 
 bot = commands.Bot(command_prefix='-', description="A fortnite server bot made by Aarrow7")
 bot.remove_command("help")
@@ -32,18 +29,11 @@ bot.remove_command("help")
 @bot.event
 async def on_ready():
   print("I'm ready to play Fortnite")
+  await bot.change_presence(game=discord.Game(name="Fortnite: Battle Royale")
   
   
 @bot.command()
 async def serverinfo(ctx):
   await ctx.send("Fortnite Battle Arena is a server for discussing stuff about Fortnite: Battle Royale, a game created by Epic Games. We here, will give you the information regarding the game, updates and much more! You can even view your fortnite stats using this bot! Great to have you here, enjoy!")
-  
-
-@bot.command()
-async def profile(ctx, platform, name):
-  player = await client.get_player(platform, name)
-  solos = await player.get_solos()
-  await ctx.send(f"# of kills in solos for {name}: {solos.kills.value}")
-  
   
 bot.run(os.environ.get("TOKEN"))
