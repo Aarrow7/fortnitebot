6950bea748570a2024cd94c9e6e28c728da44fd4
Aarrow7 <35619644+Aarrow7@users.noreply.github.com>
2018-02-09 12:45:04 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -35,3 +35,4 @@ async def serverinfo(ctx):
   await ctx.send("Fortnite Battle Arena is a server for discussing stuff about Fortnite: Battle Royale, a game created by Epic Games. We here, will give you the information regarding the game, updates and much more! You can even view your fortnite stats using this bot! Great to have you here, enjoy!")
   
   
+bot.run(os.environ.get("TOKEN"))
